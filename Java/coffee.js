e095985945a533fc225d1b69c40fd86f9b8aeb15
Hamza Qahoush <haamza.qah@gmail.com>
2021-02-23 19:20:32 +0200

    This is my Coffee Addiction Test

---
+++ Java/coffee.js
@@ -0,0 +1,13 @@
+var addict = prompt (" How many cup  of coffee do you drink ? ");
+console.log ("addict") ;
+if (addict >=3 ){
+    alert ("You are coffee Addict ")
+}
+else {
+    ("Welcome to StarBucks")
+} 
+if (addict <2){
+    alert ("Welcome to StarBucks")
+}
+var element = document.getElementById("CoffeeLOVER") ;
+        element.innerText = "Welcome to StarBucks"
