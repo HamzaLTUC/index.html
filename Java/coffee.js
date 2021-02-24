1d877baf57531d814e33d16c36e076f67d766cec
Hamza Qahoush <haamza.qah@gmail.com>
2021-02-24 17:06:33 +0200

    My functions trial

--- Java/coffee.js
+++ Java/coffee.js
@@ -1,13 +1,24 @@
-var addict = prompt (" How many cup  of coffee do you drink ? ");
-console.log ("addict") ;
-if (addict >=3 ){
-    alert ("You are coffee Addict ")
+function avgSpending (ABdunBranch , Jubayhah , CityMall){
+var sum = parseInt (ABdunBranch ) + parseInt (Jubayhah)+ parseInt(CityMall) ;
+var avg = sum/3 ;
+alert ("Your avg is"  +avg ) ;
 }
-else {
-    ("Welcome to StarBucks")
-} 
-if (addict <2){
-    alert ("Welcome to StarBucks")
+var ABdunBranch = 0 ;
+var Jubayhah = 0 ; 
+var CityMall = 0 ;
+var Counter = 0 ;
+while (Counter < 3 ){
+    if (Counter == 0) {
+        ABdunBranch = prompt (" Please enter Abdun Branch Spending :") ;
+    }
+ else if (Counter === 1 ){
+Jubayhah = prompt ('Please enter Jubayhah Branch Spending:' )
 }
-var element = document.getElementById("CoffeeLOVER") ;
-        element.innerText = "Welcome to StarBucks"
+else{
+
+    CityMall = prompt('Please enter CityMall Branch Spending:') ;
+
+}
+Counter+=1 ;
+}
+avgSpending (ABdunBranch ,Jubayhah ,  CityMall ) ;
